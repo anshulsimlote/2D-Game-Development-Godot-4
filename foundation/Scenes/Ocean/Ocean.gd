85cc943ecb3cad70555aed788481965488573276
anshulsimlote <anshulsimlote1999@gmail.com>
2026-08-04 16:11:44 +0530

    Add eating sound node and play sound on action press

--- foundation/Scenes/Ocean/Ocean.gd
+++ foundation/Scenes/Ocean/Ocean.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var plane: Sprite2D = $Plane
 @onready var heli: Sprite2D = $Helicopter
-
+@onready var eating_sound: AudioStreamPlayer = $EatingSound
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,3 +17,5 @@ func _process(delta: float) -> void:
 		plane.rotate(-1.5 * delta)
 	if Input.is_action_pressed("ui_right"):
 		plane.rotate(1.5 * delta)
+	if Input.is_action_just_pressed("ui_accept"):
+		eating_sound.play()
