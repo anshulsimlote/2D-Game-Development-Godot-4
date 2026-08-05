f7d268552650927a8885b26483e1d7e96d9fe91b
anshulsimlote <anshulsimlote1999@gmail.com>
2026-08-05 15:00:57 +0530

    Update variables script

--- foundation/Scripting/Variables.gd
+++ foundation/Scripting/Variables.gd
@@ -4,4 +4,6 @@ extends EditorScript
 
 # Called when the script is executed (using File -> Run in Script Editor).
 func _run() -> void:
-	pass
+	print("Variables!")
+	var age: int = 19
+	print("Age:",age, " Type ", type_string(typeof(age)))
