9efd6a3a82aae44791462fe9425bec4ebef39698
anshulsimlote <anshulsimlote1999@gmail.com>
2026-08-08 22:18:02 +0530

    Nodes lifecycle

--- essentials/Scripts/Hobbit.gd
+++ essentials/Scripts/Hobbit.gd
@@ -1,12 +1,20 @@
 class_name Hobbit 
 
 extends Node2D
+	
+func _init() -> void:
+	print("Hobbit In in _init"," Id: ", get_instance_id()," Name: ",name)
+	
+func _enter_tree() -> void:
+	print("Hobbit In in _enter_tree"," Id: ", get_instance_id()," Name: ",name)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	print("Hobbit In in _ready"," Id: ", get_instance_id()," Name: ",name)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotate(0.5 *delta)
+
+func _exit_tree() -> void:
+	print("Hobbit In in _exit_tree"," Id: ", get_instance_id()," Name: ",name)
--- essentials/Scripts/Wizard.gd
+++ essentials/Scripts/Wizard.gd
@@ -2,12 +2,19 @@ class_name Wizard
 
 extends Node2D
 
+func _init() -> void:
+	print("Wizard In in _init"," Id: ", get_instance_id()," Name: ",name)
+	
+func _enter_tree() -> void:
+	print("Wizard In in _enter_tree"," Id: ", get_instance_id()," Name: ",name)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	print("Wizard In in _ready"," Id: ", get_instance_id()," Name: ",name)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _exit_tree() -> void:
+	print("Wizard In in _exit_tree"," Id: ", get_instance_id()," Name: ",name)
