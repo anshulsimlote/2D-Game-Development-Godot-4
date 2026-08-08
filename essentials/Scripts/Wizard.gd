b28b26109122aa1fbfde9dcdac3c50c34de69f03
anshulsimlote <anshulsimlote1999@gmail.com>
2026-08-08 21:49:54 +0530

    Add wizard script

--- essentials/Scripts/Wizard.gd
+++ essentials/Scripts/Wizard.gd
@@ -1,6 +1,7 @@
-class_name Hobbit 
+class_name Wizard
 
 extends Node2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +10,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotate(0.5 *delta)
+	pass
