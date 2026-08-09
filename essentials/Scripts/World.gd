10801326b189aa25721c02e1203f568115849781
anshulsimlote <anshulsimlote1999@gmail.com>
2026-08-09 14:21:19 +0530

    Fetch using group

--- essentials/Scripts/World.gd
+++ essentials/Scripts/World.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-@onready var hobbitList: Array[Hobbit] = [$Hobbit,$Hobbit2,$Hobbit3]
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,7 +9,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_wizard_cast_spell() -> void:
-	for hobbit in hobbitList:
-		hobbit.scale = Vector2(0.5,0.5)
+	# Automatically finds all hobbits currently in the scene
+	get_tree().call_group("hobbits", "set_scale", Vector2(0.5, 0.5))
