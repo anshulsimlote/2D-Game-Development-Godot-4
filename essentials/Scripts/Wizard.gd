ce3ce06497548428db2c03a8e09dc705d4fa6bed
anshulsimlote <anshulsimlote1999@gmail.com>
2026-08-09 14:00:04 +0530

    Show wizard after 2 sec

--- essentials/Scripts/Wizard.gd
+++ essentials/Scripts/Wizard.gd
@@ -2,6 +2,8 @@ class_name Wizard
 
 extends Node2D
 
+var elapsed_time:float = 0.0
+
 func _init() -> void:
 	print("Wizard In in _init"," Id: ", get_instance_id()," Name: ",name)
 	
@@ -11,10 +13,13 @@ func _enter_tree() -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Wizard In in _ready"," Id: ", get_instance_id()," Name: ",name)
+	hide()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	elapsed_time += delta
+	if (elapsed_time > 2.0):
+		show()
 
 func _exit_tree() -> void:
 	print("Wizard In in _exit_tree"," Id: ", get_instance_id()," Name: ",name)
