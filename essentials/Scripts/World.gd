a56d4bd05ca174b200372c06ffb33ea92887b9c8
anshulsimlote <anshulsimlote1999@gmail.com>
2026-08-09 14:39:45 +0530

    Connect and Disconnect signal using script

--- essentials/Scripts/World.gd
+++ essentials/Scripts/World.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @onready var wizard: Wizard = $Wizard
+@onready var hobbit: Hobbit = $Hobbit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	wizard.cast_spell.connect(_on_wizard_cast_spell)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,3 +21,4 @@ func _on_hobbit_hit_wizard() -> void:
 	print("HIT WIZZARD")
 	# Automatically finds all hobbits currently in the scene
 	wizard.hit_by_hobbit()
+	hobbit.hit_wizard.disconnect(_on_hobbit_hit_wizard)
