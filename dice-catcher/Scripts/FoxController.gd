2c5886602f74897d12273f6f57af394a20cc91af
anshulsimlote <anshulsimlote1999@gmail.com>
2026-08-16 21:42:16 +0530

    Add eat sound

--- dice-catcher/Scripts/FoxController.gd
+++ dice-catcher/Scripts/FoxController.gd
@@ -3,6 +3,7 @@ extends Area2D
 class_name FoxController
 @onready var fox: Sprite2D = $Fox
 @export var fox_speed: float = 200.0
+@onready var eat_sound: AudioStreamPlayer = $EatSound
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,3 +18,10 @@ func move(delta: float) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	move(delta)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is DiscController:
+		print("EAT")
+		area.queue_free()
+		eat_sound.play()
