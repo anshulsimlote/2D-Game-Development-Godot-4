ea3fa976e5ed278ebf3416d30fe2d381854f1e1f
anshulsimlote <anshulsimlote1999@gmail.com>
2026-08-16 21:20:39 +0530

    Fox script enhancement

--- dice-catcher/Scripts/FoxController.gd
+++ dice-catcher/Scripts/FoxController.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 class_name FoxController
-
+@onready var fox: Sprite2D = $Fox
 @export var fox_speed: float = 200.0
 
 # Called when the node enters the scene tree for the first time.
@@ -9,15 +9,11 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func move(delta: float) -> void:
-	var move_left:bool = Input.is_action_pressed("ui_left")
-	var move_right:bool = Input.is_action_pressed("ui_right")
+	var move_dir:float = Input.get_axis("ui_left","ui_right")
 	var move_speed:float = fox_speed * delta
-	var move_dir:float = 0.0
-	
-	if move_left: move_dir -= 1.0
-	if move_right: move_dir += 1.0
-	
 	move_local_x(move_speed * move_dir)
+	if !is_zero_approx(move_dir):
+		fox.flip_h = move_dir > 0.0 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	move(delta)
