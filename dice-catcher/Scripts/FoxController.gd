3941009562bec2822059d2c0c3c7da9765051814
anshulsimlote <anshulsimlote1999@gmail.com>
2026-08-16 21:14:37 +0530

    Fox script update

--- dice-catcher/Scripts/FoxController.gd
+++ dice-catcher/Scripts/FoxController.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 class_name FoxController
 
@@ -9,12 +9,15 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func move(delta: float) -> void:
+	var move_left:bool = Input.is_action_pressed("ui_left")
+	var move_right:bool = Input.is_action_pressed("ui_right")
 	var move_speed:float = fox_speed * delta
-	if(Input.is_action_pressed("ui_right")):
-		move_local_x(move_speed)
-	if(Input.is_action_pressed("ui_left")):
-		move_local_x(-move_speed)
+	var move_dir:float = 0.0
 	
+	if move_left: move_dir -= 1.0
+	if move_right: move_dir += 1.0
+	
+	move_local_x(move_speed * move_dir)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	move(delta)
